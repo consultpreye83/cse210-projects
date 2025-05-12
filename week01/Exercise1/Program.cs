9f93cac965981be558e6c653bc024789249d0101
Suowari Preye <consultpreye83@gmail.com>
2025-05-12 19:40:06 +0100

    week01/exercise01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // interact with the user by asking for their name.
+        Console.Write("What is your first name? ");
+        String First = Console.ReadLine ();
+
+        Console.Write("What is your Last Name? ");
+        string Last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Last}, {First} {Last}");
     }
 }
